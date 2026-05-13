6b484cabfc73782b6d1109e50a346c18a15a16a7
farhan-fadillah <farhantek26@gmail.com>
2026-05-13 10:30:40 +0700

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // Anda bisa menambahkan palet warna spesifik Kemenkumham di sini nanti
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
